1312808c7b7752db2a327a0a6c3a7d3ac857cbf2
Suzuki Takafumi <suzukitakafumifcc@gmail.com>
2024-11-11 15:50:28 +0900

    fix: model label

--- app/Filament/Resources/PatientResource.php
+++ app/Filament/Resources/PatientResource.php
@@ -19,6 +19,8 @@ class PatientResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
+    protected static ?string $modelLabel = '患者';
+
     public static function form(Form $form): Form
     {
         return $form
