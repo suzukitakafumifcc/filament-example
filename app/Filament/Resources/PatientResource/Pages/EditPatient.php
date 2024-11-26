fbbdaa890f0d48c7f84bb2de67eb8fe026d9ec91
Suzuki Takafumi <suzukitakafumifcc@gmail.com>
2024-11-26 16:23:12 +0900

    feat: patients

--- app/Filament/Resources/PatientResource/Pages/EditPatient.php
+++ app/Filament/Resources/PatientResource/Pages/EditPatient.php
@@ -3,7 +3,9 @@
 namespace App\Filament\Resources\PatientResource\Pages;
 
 use App\Filament\Resources\PatientResource;
+use App\Models\Patient;
 use Filament\Actions;
+use Filament\Actions\Action;
 use Filament\Resources\Pages\EditRecord;
 
 class EditPatient extends EditRecord
@@ -14,6 +16,12 @@ class EditPatient extends EditRecord
     {
         return [
             Actions\DeleteAction::make(),
+            Action::make('更新')
+                ->action(function (Patient $record) {
+                    $this->refreshFormData([
+                        'type',
+                    ]);
+                }),
         ];
     }
 }
--- app/Filament/Resources/PatientResource/Pages/ListPatients.php
+++ app/Filament/Resources/PatientResource/Pages/ListPatients.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\PatientResource\Pages;
 
 use App\Filament\Resources\PatientResource;
+use App\Filament\Widgets\TreatmentChart;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
@@ -14,6 +15,13 @@ class ListPatients extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            TreatmentChart::class,
         ];
     }
 }
